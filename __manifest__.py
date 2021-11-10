a9491b4984f93f441b5ba16ff822f1785eb138b6
KRAKOU Danho Guy Sylvestre <guysylvestre876@gmail.com>
2021-11-10 18:20:03 +0100

    Creation de vue de recherche et de vue kanban

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hospital_management",
+    'summary': """Hospîtal Management Software""",
+    'description': """Hospîtal Management Software""",
+    'author': "Danho Guy Sylvestre KRAKOU",
+    'website': "http://www.yourcompany.com",
+    'category': 'Productivity',
+    'version': '1.0',
+    'licence': 'LGPL-3',
+    'depends': ['sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/patient.xml',
+        'views/sale.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
